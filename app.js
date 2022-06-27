8c3097d71df0ea19955ca067aaf4c58ec6923aff
wkchiuea <103587188+wkchiuea@users.noreply.github.com>
2022-06-27 12:03:22 +0800

    add get post delete api

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ const Article = mongoose.model("Article", articleParser);
 
 
 
+app.get("/articles", (req, res) => {
+    Article.find(function(err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+app.post("/articles", (req, res) => {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save((err) => {
+        if (!err) {
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.delete("/articles", (req, res) => {
+    Article.deleteMany(function(err) {
+        res.send((!err) ? "Successfully deleted all articles." : err);
+    })
+});
 
 
 
